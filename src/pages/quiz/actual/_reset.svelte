f34b262cdd460bef60f78c3b139b6dd603da4ea5
Wolfr <johan.ronsse@gmail.com>
2020-01-16 22:19:39 +0100

    Test for jakobdk
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/pages/quiz/actual/_reset.svelte
+++ src/pages/quiz/actual/_reset.svelte
@@ -1,17 +1,1 @@
-<script>
-  import { url, params } from '@sveltech/routify';
-  import Quiz from '../../../Quiz.svelte';
-  import ContentArea from '../../../UI/ContentArea.svelte';
-  import { quizSettings } from '../../../stores/quizsettings.js';
-
-</script>
-
-<svelte:head>
-    <title>Quiz - Kana Master Web</title>
-</svelte:head>
-
-<div class="c-scroll-context">
-    <ContentArea spacing>
-        <Quiz />
-    </ContentArea>
-</div>
+Oi, hey
